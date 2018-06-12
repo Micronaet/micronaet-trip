ec9748fa5abe7154800bae77ba24d593955e4b11
nicothebrush <nicola.riolini@gmail.com>
2018-06-12 13:12:13 +0200

    generic bugfix (fast message)

--- account_trip_edi_check_invoice/wizard/load_ddt_wizard.py
+++ account_trip_edi_check_invoice/wizard/load_ddt_wizard.py
@@ -203,6 +203,7 @@ class EdiLoadDdtLineWizard(orm.TransientModel):
         # Print sorted order:
         for order in sorted(res, key=lambda x: x.name):            
             # DDT Line color depend on difference: (red - green - white)
+            import pdb; pdb.set_trace()
             difference = res[order][0]
             if abs(difference) <= tollerance:
                 f_text_default = f_bg_white
