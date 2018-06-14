929ca2dde6843cd976b50f298a696a426d91e755
nicothebrush <nicola.riolini@gmail.com>
2018-06-14 08:20:12 +0200

    generic bugfix (fast message)

--- account_trip_edi_check_invoice/wizard/load_ddt_wizard.py
+++ account_trip_edi_check_invoice/wizard/load_ddt_wizard.py
@@ -332,7 +332,7 @@ class EdiLoadDdtLineWizard(orm.TransientModel):
                 default_format=f_bg_green_number, col=col)
 
         return excel_pool.return_attachment(cr, uid, ws_name, 
-            version='7.0', php=True, context=context)
+            version='7.0', php=False, context=context)
 
     _columns = {
         'mode': fields.selection([
