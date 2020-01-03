d09a1289d7de02423f3d3dd8bff2c08ea988e0f4
nicothebrush <nicola.riolini@gmail.com>
2020-01-03 14:24:16 +0100

    generic bugfix (fast message)

--- account_trip_edi_xlsx_report/wizard/export_document.py
+++ account_trip_edi_xlsx_report/wizard/export_document.py
@@ -199,12 +199,6 @@ class QualityExportExcelReport(orm.TransientModel):
         format_number_green = excel_pool.get_format('number_green')
         format_number_blue = excel_pool.get_format('number_blue')
         format_number_red = excel_pool.get_format('number_red')
-
-        #format_number = excel_pool.get_format('text')
-        #format_number_green = excel_pool.get_format('text_green')
-        #format_number_blue = excel_pool.get_format('text_blue')
-        #format_number_red = excel_pool.get_format('text_red')
-
         
         # Column setup width:
         col_width = [
