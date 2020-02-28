d9d02a486802fb3020ae5c31d9639322dc8a40e4
nicothebrush <nicola.riolini@gmail.com>
2020-02-28 08:29:03 +0100

    generic bugfix (fast message)

--- account_trip_edi/stock_status_report.py
+++ account_trip_edi/stock_status_report.py
@@ -584,7 +584,8 @@ class edi_company_report(orm.Model):
             item[5:] for item in sorted(report['header'].keys())
             ], excel_format['header'], col=fixed_cols)
         excel_pool.autofilter(ws_name, row, 0, row, 2)    
-        excel_pool.freeze_panes(ws_name, row, 0) # Lock title row
+        excel_pool.freeze_panes(ws_name, row + 1, 0) # Lock title row
+        excel_pool.freeze_panes(ws_name, 0, 3) # Lock title row
 
         # ---------------------------------------------------------------------        
         # Data
