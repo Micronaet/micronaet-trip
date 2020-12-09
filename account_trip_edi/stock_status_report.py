d54d2bdfa2441802be333b131a0b668c296df922
nicothebrush <nicola.riolini@gmail.com>
2020-12-09 18:08:39 +0100

    generic bugfix (fast message)

--- account_trip_edi/stock_status_report.py
+++ account_trip_edi/stock_status_report.py
@@ -452,6 +452,7 @@ class edi_company_report(orm.Model):
                     clean_name += c
                 else:
                     clean_name += '[ERR]'
+                    pdb.set_trace()
 
             account_data[default_code] = [
                 clean_name,
