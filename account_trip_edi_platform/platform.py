6d0cf9335287034fcbd26953677dd3930a89ef49
nicothebrush <nicola.riolini@gmail.com>
2021-05-29 00:19:53 +0200

    add data structure for supplier order

--- account_trip_edi_platform/platform.py
+++ account_trip_edi_platform/platform.py
@@ -40,6 +40,22 @@ from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
 _logger = logging.getLogger(__name__)
 
 
+class EdiCompany(orm.Model):
+    """ Model name: Edi Company
+    """
+    _inherit = 'edi.company'
+
+    def import_platform_supplier_order(self, cr, uid, ids, context=None):
+        """ Import supplier order from platform
+        """
+
+    _columns = {
+        'has_platfrom': fields.Boolean('Has platform'),
+        'connection_id': fields.many2one(
+            'http.request.connection', 'Connection'),
+    }
+
+
 class EdiSupplierOrder(orm.Model):
     """ Model name: Edi Supplier Order
     """
@@ -50,8 +66,65 @@ class EdiSupplierOrder(orm.Model):
     _order = 'name'
 
     _columns = {
+        'name': fields.char('Numero ordine', size=30, required=True),
+        'supplier_code': fields.char('Codice produttore', size=30, required=True),
+        'dealer': fields.char('Concessionario', size=30),
+        'dealer_code': fields.char('Codice Concessionario', size=30),
+        'supplier': fields.char('Ragione sociale produttore', size=40),
+        'order_date': fields.char('Data ordine', size=20),
+        'deadline_date': fields.char('Data consegna richiesta', size=20),
+        'note': fields.text('Nota ordine'),
+    }
+# 'NUMERO_ORDINE': u'210083054_000', 'RAGIONE_SOCIALE_PRODUTTORE':
+# u'PANAPESCA SPA', 'DATA_ORDINE': u'20210510', 'DATA_CONSEGNA_RICHIESTA':
+# u'20210505', 'NOTA_ORDINE': u'',
+# 'CONCESSIONARIO': u'PANAPESCA SPA' 'CODICE_CONCESSIONARIO': u'1335',
+# 'CODICE_PRODUTTORE': u'ITA000061',
+
+
+class EdiSupplierOrderLine(orm.Model):
+    """ Model name: Edi Supplier Order Line
+    """
+
+    _name = 'edi.supplier.order.line'
+    _description = 'Supplier order line'
+    _rec_name = 'name'
+    _order = 'name'
+
+    _columns = {
+        'order_id': fields.many2one('edi.supplier.order', 'Ordine produttore'),
         'name': fields.char(
-            'Name', size=64, required=True,
-            ),
+            'Descrizione articolo', size=40, required=True),
+        'supplier_name': fields.char(
+            'Descrizione articolo produttore', size=40),
+        'sequence': fields.char('Seq.', size=4),
+        'supplier_code': fields.char('Codice produttore', size=20),
+        'code': fields.char('Codice articolo', size=20),
+        'uom_supplier': fields.char('UM fornitore', size=10),
+        'uom_product': fields.char('UM prodotto', size=10),
+        'product_qty': fields.char('Q.', size=20),  # todo change in float
+        'order_product_qty': fields.char('Q.', size=20),
+        'note': fields.char('Nota riga', size=40),
+    }
+
+# 'RIGA_ORDINE': u'1', 'CODICE_ARTICOLO_PRODUTTORE': u'419490',
+# 'CODICE_ARTICOLO': u'AV030149',
+# 'UM_ARTICOLO_PRODUTTORE': u'KG', 'DESCRIZIONE_ARTICOLO_PRODUTTORE':
+# u'FILETTI DI PANGASIO CONGELATI',
+# 'QTA_ORDINE': u'00000004700000', 'UM_ARTICOLO': u'KG',
+# 'QTA_ORDINE_PRODUTTORE': u'00000004700000',
+# 'NOTA_RIGA': u'', 'DESCRIZIONE_ARTICOLO': u'PANGASIO FILETTI
+# SP 100-180GR GELO',
+
+
+class EdiSupplierOrderRelation(orm.Model):
+    """ Model name: Edi Supplier Order relation
+    """
+
+    _inherit = 'edi.supplier.order'
+
+    _columns = {
+        'line_ids': fields.one2many(
+            'edi.supplier.order.line', 'order_id', 'Righe')
     }
 
