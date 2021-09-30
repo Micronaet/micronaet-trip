02addfb8fb8e9397750e6ddc175089a544d29366
nicothebrush <nicola.riolini@gmail.com>
2021-09-30 12:27:08 +0200

    generic bugfix (fast message)

--- account_trip_edi_platform/script/portal_of_to_account.py
+++ account_trip_edi_platform/script/portal_of_to_account.py
@@ -19,6 +19,15 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
+import os
+import sys
+import erppeek
+from datetime import datetime, timedelta
+import ConfigParser
+
+# -----------------------------------------------------------------------------
+# Read configuration parameter:
+# -----------------------------------------------------------------------------
 # From config file:
 cfg_file = os.path.expanduser('./company.cfg')
 
