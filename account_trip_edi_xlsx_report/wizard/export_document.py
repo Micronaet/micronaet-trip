dd2e5ebb9c0020ce9b897999ebf67295e919b453
nicothebrush <nicola.riolini@gmail.com>
2021-11-17 09:26:17 +0100

    generic bugfix (fast message)

--- account_trip_edi_xlsx_report/wizard/export_document.py
+++ account_trip_edi_xlsx_report/wizard/export_document.py
@@ -351,6 +351,7 @@ class QualityExportExcelReport(orm.TransientModel):
                 data[13] = (sign * clean_float(item[3]), f_number)
                 data[14] = (clean_float(item[4]), f_number)
                 data[15] = (clean_float(item[5]), f_number)
+                pdb.set_trace()
 
                 key = (data[10], data[11], data[12])
 
