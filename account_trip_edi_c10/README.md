272fa12a13e3b48eb26c12aab72070552f2aa837
nicothebrush <nicola.riolini@gmail.com>
2021-11-24 12:55:08 +0100

    [ADD] C9: HOS

---
+++ account_trip_edi_c10/README.md
@@ -0,0 +1,7 @@
+=========
+Company 8
+=========
+
+Company 8 is the same of Company 8
+Except that work with different folder
+
---
+++ account_trip_edi_c10/mail.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# OpenERP, Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+import os
+import pdb
+import sys
+import logging
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from openerp.tools.translate import _
+
+_logger = logging.getLogger(__name__)
+
+
+class edi_company_c10(orm.Model):
+    """ Add model for parametrize function for Company 10
+        Model has only function for a sort of abstract class
+    """
+    _inherit = 'edi.company.c10'
+
+    # -------------------------------------------------------------------------
+    #                     Abstract function for mail:
+    # -------------------------------------------------------------------------
+    def get_order_number(self, record):
+        """ EDI mail: Extract order number
+            Format: 'Ordine n.30451/21 - VETROPACK - RISTORAZIONE - 29/11/2021'
+        """
+        subject = record['Subject']
+        return subject.split('-')[0].split('.').strip()#  .replace('/', '-')
