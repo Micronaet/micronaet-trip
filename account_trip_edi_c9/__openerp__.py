5780a93fdbc6325173d99816f5db61aa53fa12cc
nicothebrush <nicola.riolini@gmail.com>
2021-11-24 11:05:16 +0100

    generic bugfix (fast message)

--- account_trip_edi_c9/__openerp__.py
+++ account_trip_edi_c9/__openerp__.py
@@ -18,12 +18,12 @@
 ###############################################################################
 
 {
-    'name': 'Account trip - EDI order import procedure for Company 8',
+    'name': 'Account trip - EDI order import procedure for Company 9',
     'version': '0.1',
     'category': 'EDI',
     'description': """
         Import function for load EDI files
-        this is a model of importation for Company 8
+        this is a model of importation for Company 9
         This type of module are linked to edi.company for parametrize
         function for importation.
         Import procedure is in account_trip_edi 
