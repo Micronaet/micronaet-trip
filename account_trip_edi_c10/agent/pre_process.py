192ae5c5acd1ae4620a20377de0fd57b955a4fc9
nicothebrush <nicola.riolini@gmail.com>
2021-11-25 16:23:09 +0100

    generic bugfix (fast message)

--- account_trip_edi_c10/agent/pre_process.py
+++ account_trip_edi_c10/agent/pre_process.py
@@ -90,7 +90,7 @@ def clean_text(text, length, uppercase=False, error=None, truncate=False):
 def clean_date(company_date):
     """ Current format: YYMMDD
     """
-    return company_date.strip()
+    return '20%s' % company_date.strip()
 
 
 def clean_float(
