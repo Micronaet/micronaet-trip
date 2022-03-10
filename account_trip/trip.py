f7db418851fd2e43dff0e9c5384a4c5202aa32a4
nicothebrush <nicola.riolini@gmail.com>
2022-03-10 12:02:06 +0100

    generic bugfix (fast message)

--- account_trip/trip.py
+++ account_trip/trip.py
@@ -359,7 +359,7 @@ class trip_order(orm.Model):
                     error = ''
                     name = sql_pool.KEY_OC_FORMAT % record
                     number = str(record['NGL_DOC'])
-                    date = record['DTT_COD'].strptime(
+                    date = record['DTT_COD'].strftime(
                         DEFAULT_SERVER_DATE_FORMAT)
                     tour_name = record['CDS_NOTE'].strip()
                     tour_id = tour_pool.search_tour(
