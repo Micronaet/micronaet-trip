3b01d86e31fb00b4e2f01b23b9bc6f4ea6c00259
nicothebrush <nicola.riolini@gmail.com>
2022-03-10 18:08:25 +0100

    generic bugfix (fast message)

--- account_trip/trip.py
+++ account_trip/trip.py
@@ -512,9 +512,14 @@ class trip_order(orm.Model):
 
             # Import line detail for order:
             # todo parameter the filename:
+            pdb.set_trace()
             filename = os.path.expanduser('~/mexal/viaggi/freschi.txt')
             _logger.info('Updating extra info, file: %s' % filename)
+            counter = 1
             for line in open(filename, 'r'):
+                counter += 1
+                if not counter % 10:
+                    _logger('%s line read' % counter)
                 row = line.strip().split(';')
                 if len(row) != 3:
                     _logger.error('Line empty, jumped')
@@ -532,6 +537,7 @@ class trip_order(orm.Model):
                     _logger.error(
                         'Cannot load extra info for order: %s' % number)
             _logger.info('All trip order is updated!')
+            pdb.set_trace()
 
             if error_block['master']:
                 company_pool.write(cr, uid, [company_id], {
