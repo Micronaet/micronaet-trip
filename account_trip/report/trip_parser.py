cd4c270bddead57f8a68b3175f780939de62603d
nicothebrush <nicola.riolini@gmail.com>
2022-03-11 17:37:13 +0100

    generic bugfix (fast message)

--- account_trip/report/trip_parser.py
+++ account_trip/report/trip_parser.py
@@ -79,9 +79,15 @@ class Parser(rml_parse):
                     'quantity': 0.0,
                 }
                 for order in orders:
-                    extra['time'].add((order.time or '').strip())
-                    extra['deadline'].add((order.date or '').strip())
-                    extra['number'].add(order.name.split('-')[-1].strip())
+                    time = (order.time or '').strip()
+                    if time:
+                        extra['time'].add(time)
+                    deadline = (order.date or '').strip()
+                    if deadline:
+                        extra['deadline'].add(deadline)
+                    number = order.name.split('-')[-1].strip()
+                    if number:
+                        extra['number'].add(number)
                     extra['quantity'] += order.prevision_load
 
                 extra['time'] = ' '.join(extra['time'])
