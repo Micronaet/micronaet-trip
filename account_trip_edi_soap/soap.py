3a2fb73629abfdee80b9ba11a0f6f4521c996d7f
nicothebrush <nicola.riolini@gmail.com>
2022-08-16 09:03:13 +0200

    generic bugfix (fast message)

--- account_trip_edi_soap/soap.py
+++ account_trip_edi_soap/soap.py
@@ -837,8 +837,8 @@ class EdiSoapConnection(orm.Model):
             except:
                 raise osv.except_osv(
                     _('Errore di connessione con MSC'),
-                    _('Verificare se il portale è operativo o se è ' 
-                      'scaduto il codice "secret", richidere a MSC o Niuma!'),
+                    _('Verificare se il portale MCS è operativo o se è ' 
+                      'scaduto il codice "secret", richiedere a MSC o Niuma!'),
                 )
             if response_json['success']:
                 res = response_json.get('reply', {}).get('res')
