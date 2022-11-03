3a715cf828e5e68102824fac2cc74bdba5f0e153
nicothebrush <nicola.riolini@gmail.com>
2022-11-03 09:08:51 +0100

    generic bugfix (fast message)

--- account_trip_edi_c11/edi.py
+++ account_trip_edi_c11/edi.py
@@ -50,7 +50,7 @@ class edi_company_c11(orm.Model):
         'detail_description': (179, 239),
         'detail_um': (654, 669),  # OK
         'detail_quantity': (652, 664),  # OK
-        'detail_price': (0, 0),  # Not present
+        'detail_price': (0, 0),  # todo add from partic
         'detail_total': (0, 0),  # Not present
 
         # Destination blocks:
