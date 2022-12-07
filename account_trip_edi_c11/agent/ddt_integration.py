db355628a2e48b013f89abf59e9b2be9643ddabc
nicothebrush <nicola.riolini@gmail.com>
2022-12-07 13:24:28 +0100

    generic bugfix (fast message)

--- account_trip_edi_c11/agent/ddt_integration.py
+++ account_trip_edi_c11/agent/ddt_integration.py
@@ -310,7 +310,7 @@ for root, folders, files in os.walk(from_path):
             new_f.write(new_row)
         new_f.close()
         if total['error'] > 0:
-            print('>> %s\nRighe: %s\nErrore: %s\nFatte: %s' % (
+            print('>> %s\nRighe: %s\nErrore: %s\nFatte: %s\n\n' % (
                 filename, total['total'], total['error'], total['done'],
             ))
     break  # No subfolder
