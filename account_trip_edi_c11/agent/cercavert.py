33a68bd6bff54972506126861dec924e5c274480
nicothebrush <nicola.riolini@gmail.com>
2022-12-16 10:49:06 +0100

    generic bugfix (fast message)

--- account_trip_edi_c11/agent/cercavert.py
+++ account_trip_edi_c11/agent/cercavert.py
@@ -219,11 +219,12 @@ for root, dirs, files in os.walk(in_history):
 
             key = (data[order_file]['header']['order'], detail['code'])
 
-            text_line = '%-30s|%-20s|%-30s|%-12s|%-4s|%-4s\r\n' % (
+            mexal_line = mexal_db.get(key, '')
+            text_line = '%-20s|%-20s|%-30s|%-12s|%-4s|%-4s\r\n' % (
                 # Mexal
                 clean_text(
-                    mexal_db.get(key, ''),
-                    30, error=error, truncate=True),
+                    mexal_line,
+                    20, error=error, truncate=True),
 
                 clean_text(
                     data[order_file]['header']['order'],
