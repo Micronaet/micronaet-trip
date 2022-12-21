1375d1c8f0d70b828ad9de5dec2e17420a0dfa58
nicothebrush <nicola.riolini@gmail.com>
2022-12-21 09:41:45 +0100

    generic bugfix (fast message)

--- account_trip_edi_c11/agent/cercavert.py
+++ account_trip_edi_c11/agent/cercavert.py
@@ -116,120 +116,154 @@ extension = 'txt'
 # -----------------------------------------------------------------------------
 # Read IN folder:
 # -----------------------------------------------------------------------------
-mode = 'ddt'
-mexal_db = {}
-for line in open('/home/openerp/etl/edi/dussmann/check/%s.csv' % mode, 'r'):
-    line = line.strip()
-    row = line.split(';')
-    reference = row[0].strip()
-    order = row[1].strip().replace('-', '')
-    if not order:
-        print('No order jump line')
-        continue
-    code = row[2].strip()
-    key = (order, code)
-    mexal_db[key] = reference  # reference  # Save all line
-
-file_original = \
-    '/home/openerp/etl/edi/dussmann/check/integrazioni_%s.log' % mode
-f_out = open(file_original, 'w')
-
-data = {}
-for root, dirs, files in os.walk(in_history):
-    for f in files:
-        if not f.lower().endswith(extension):
+for mode in ('ddt', 'oc'):
+    mexal_db = {}
+    for line in open(
+            '/home/openerp/etl/edi/dussmann/check/%s.csv' % mode, 'r'):
+        line = line.strip()
+        row = line.split(';')
+        reference = row[0].strip()
+        order = row[1].strip().replace('-', '')
+        if not order:
+            print('No order jump line')
             continue
+        code = row[2].strip()
+        key = (order, code)
+        mexal_db[key] = reference  # reference  # Save all line
 
-        # Fullname needed:
-        command = 'NEW'  # Only this!
-        file_in = os.path.join(root, f)
-        create_date = datetime.fromtimestamp(
-            os.path.getctime(file_in)).strftime('%Y%m%d_%H%M%S')
+    file_original = \
+        '/home/openerp/etl/edi/dussmann/check/integrazioni_%s.log' % mode
+    f_out = open(file_original, 'w')
 
-        # ---------------------------------------------------------------------
-        # Read input file:
-        # ---------------------------------------------------------------------
-        error = []
-        f_in = open(file_in, 'r')
+    data = {}
+    for root, dirs, files in os.walk(in_history):
+        for f in files:
+            if not f.lower().endswith(extension):
+                continue
 
-        counter = 0
-        for line in f_in:
-            # Clean base 64 data (clean start editor char):
-            line = line.replace('\xff', '').replace('\xfe', '').replace(
-                '\x00', '')
-            if not line:
-                continue  # Jump empty line
+            # Fullname needed:
+            command = 'NEW'  # Only this!
+            file_in = os.path.join(root, f)
+            create_date = datetime.fromtimestamp(
+                os.path.getctime(file_in)).strftime('%Y%m%d_%H%M%S')
 
-            # -----------------------------------------------------------------
-            # Create file every code break:
-            # -----------------------------------------------------------------
-            order_year = line[3:7].strip()
-            order_number = line[7:13].strip()
+            # ---------------------------------------------------------------------
+            # Read input file:
+            # ---------------------------------------------------------------------
+            error = []
+            f_in = open(file_in, 'r')
 
-            # Generate filename for every order included:
-            order_file = '%s_%s_%s_%s.%s' % (
-                order_year,  # Year
-                order_number,  # Number
-                create_date,  # Date of file
-                command,  # Mode
-                extension,   # Extension same as original
-            )
+            counter = 0
+            for line in f_in:
+                # Clean base 64 data (clean start editor char):
+                line = line.replace('\xff', '').replace('\xfe', '').replace(
+                    '\x00', '')
+                if not line:
+                    continue  # Jump empty line
 
-            if order_file not in data:
-                data[order_file] = {
-                    'counter': 0,
-                    'line': [],
-                    'header': {
-                        'type': line[:2].strip(),  # AA
-                        # 'sequence': line[2:7].strip(),
-                        'order': '%s%s' % (order_year, order_number),
-                        # 'date': line[13:21].strip(),
-                        # 'deadline': line[644:652].strip(),
+                # -----------------------------------------------------------------
+                # Create file every code break:
+                # -----------------------------------------------------------------
+                order_year = line[3:7].strip()
+                order_number = line[7:13].strip()
 
-                        'stock_type': line[25:26].strip(),
-                        'company_code': line[26:33].strip(),
-                        'destination_code': line[21:25].strip(),
-                        'destination_address_code': line[33:37].strip(),
+                # Generate filename for every order included:
+                order_file = '%s_%s_%s_%s.%s' % (
+                    order_year,  # Year
+                    order_number,  # Number
+                    create_date,  # Date of file
+                    command,  # Mode
+                    extension,   # Extension same as original
+                )
 
-                        # # 'document': line[67:69].strip(),
-                        # # 'destination_facility': (26, 33),  # OK Stock code
-                        # # 'destination_cost': (21, 25),  # OK CDC
-                        # # 'destination_site': (33, 37),  # OK Address code
-                        # 'destination_description': line[37:97].strip(),
-                        # 'cig': line[719:744].strip(),
-                    },
-                }
+                if order_file not in data:
+                    data[order_file] = {
+                        'counter': 0,
+                        'line': [],
+                        'header': {
+                            'type': line[:2].strip(),  # AA
+                            # 'sequence': line[2:7].strip(),
+                            'order': '%s%s' % (order_year, order_number),
+                            # 'date': line[13:21].strip(),
+                            # 'deadline': line[644:652].strip(),
 
-            # Detail lines:
-            detail = {
-                # 'type': '',  # line[:2].strip(),
-                'sequence': line[278:282].strip(),
-                'code': line[614:644].strip(),
-                # 'name': line[359:614].strip(),
-                # 'uom': line[664:669].strip(),
-                'quantity': line[652:664].strip(),
-                # 'price': '',   # line[72:81].strip(),
-                # 'vat': '',   # line[67:69].strip(),
-                }
+                            'stock_type': line[25:26].strip(),
+                            'company_code': line[26:33].strip(),
+                            'destination_code': line[21:25].strip(),
+                            'destination_address_code': line[33:37].strip(),
 
-            # -----------------------------------------------------------------
-            # Convert row input file:
-            # -----------------------------------------------------------------
-            data[order_file]['counter'] += 1
-            counter = str(data[order_file]['counter'])
+                            # # 'document': line[67:69].strip(),
+                            # # 'destination_facility': (26, 33),  # OK Stock code
+                            # # 'destination_cost': (21, 25),  # OK CDC
+                            # # 'destination_site': (33, 37),  # OK Address code
+                            # 'destination_description': line[37:97].strip(),
+                            # 'cig': line[719:744].strip(),
+                        },
+                    }
 
-            key = (data[order_file]['header']['order'], detail['code'])
-            mexal_line = mexal_db.get(key, '')
-            if mexal_line:
-                text_line = '%-20s|%-20s|%-30s|%-12s|%-4s|%-4s\r\n' % (
-                    # Mexal
-                    clean_text(
-                        mexal_line,
-                        20, error=error, truncate=True),
+                # Detail lines:
+                detail = {
+                    # 'type': '',  # line[:2].strip(),
+                    'sequence': line[278:282].strip(),
+                    'code': line[614:644].strip(),
+                    # 'name': line[359:614].strip(),
+                    # 'uom': line[664:669].strip(),
+                    'quantity': line[652:664].strip(),
+                    # 'price': '',   # line[72:81].strip(),
+                    # 'vat': '',   # line[67:69].strip(),
+                    }
 
+                # -----------------------------------------------------------------
+                # Convert row input file:
+                # -----------------------------------------------------------------
+                data[order_file]['counter'] += 1
+                counter = str(data[order_file]['counter'])
+
+                key = (data[order_file]['header']['order'], detail['code'])
+                mexal_line = mexal_db.get(key, '')
+                if mexal_line:
+                    text_line = '%-20s|%-20s|%-30s|%-12s|%-4s|%-4s\r\n' % (
+                        # Mexal
+                        clean_text(
+                            mexal_line,
+                            20, error=error, truncate=True),
+
+                        clean_text(
+                            data[order_file]['header']['order'],
+                            20, error=error, truncate=True),
+                        clean_text(
+                            detail['code'],
+                            30, error=error, truncate=True),
+                        clean_text(
+                            detail['quantity'],
+                            12, error=error, truncate=True),
+                        clean_text(
+                            counter,
+                            4, error=error, truncate=True),
+                        clean_text(
+                            detail['sequence'],
+                            4, error=error, truncate=True),
+                        )
+                    f_out.write(text_line)
+
+                '''# Only subtype
+                text_line = '%-20s|%-1s|%-7s|%-4s|%-4s|%-30s|%-12s|%-4s|%4s|%-2s' \
+                            '\r\n' % (
                     clean_text(
                         data[order_file]['header']['order'],
                         20, error=error, truncate=True),
+                    clean_text(
+                        data[order_file]['header']['stock_type'],
+                        1, error=error, truncate=True),
+                    clean_text(
+                        data[order_file]['header']['company_code'],
+                        7, error=error, truncate=True),
+                    clean_text(
+                        data[order_file]['header']['destination_code'],
+                        4, error=error, truncate=True),
+                    clean_text(
+                        data[order_file]['header']['destination_address_code'],
+                        4, error=error, truncate=True),
                     clean_text(
                         detail['code'],
                         30, error=error, truncate=True),
@@ -242,47 +276,14 @@ for root, dirs, files in os.walk(in_history):
                     clean_text(
                         detail['sequence'],
                         4, error=error, truncate=True),
+    
+                    clean_text(
+                        data[order_file]['header']['type'],
+                        2, error=error, truncate=True),
+                    # price: added in spooler procedure
                     )
-                f_out.write(text_line)
-
-            '''# Only subtype
-            text_line = '%-20s|%-1s|%-7s|%-4s|%-4s|%-30s|%-12s|%-4s|%4s|%-2s' \
-                        '\r\n' % (
-                clean_text(
-                    data[order_file]['header']['order'],
-                    20, error=error, truncate=True),
-                clean_text(
-                    data[order_file]['header']['stock_type'],
-                    1, error=error, truncate=True),
-                clean_text(
-                    data[order_file]['header']['company_code'],
-                    7, error=error, truncate=True),
-                clean_text(
-                    data[order_file]['header']['destination_code'],
-                    4, error=error, truncate=True),
-                clean_text(
-                    data[order_file]['header']['destination_address_code'],
-                    4, error=error, truncate=True),
-                clean_text(
-                    detail['code'],
-                    30, error=error, truncate=True),
-                clean_text(
-                    detail['quantity'],
-                    12, error=error, truncate=True),
-                clean_text(
-                    counter,
-                    4, error=error, truncate=True),
-                clean_text(
-                    detail['sequence'],
-                    4, error=error, truncate=True),
-
-                clean_text(
-                    data[order_file]['header']['type'],
-                    2, error=error, truncate=True),
-                # price: added in spooler procedure
-                )
-                '''
-        f_in.close()
-        if error:
-            continue
+                    '''
+            f_in.close()
+            if error:
+                continue
 
