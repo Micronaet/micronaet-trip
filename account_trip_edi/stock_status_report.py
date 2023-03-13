df9c8358e89d4bf581293053db6ddd99c12bafc6
nicothebrush <nicola.riolini@gmail.com>
2023-03-13 11:11:22 +0100

    generic bugfix (fast message)

--- account_trip_edi/stock_status_report.py
+++ account_trip_edi/stock_status_report.py
@@ -732,19 +732,19 @@ class edi_company_report(orm.Model):
                 color = black
 
             excel_pool.write_xls_line(ws_name, row, [
-                #(u'Neg.' if has_negative else u'Pos.', color['text']),
-                #(self.get_product_category(default_code), color['text']),
+                (u'Neg.' if has_negative else u'Pos.', color['text']),
+                (self.get_product_category(default_code), color['text']),
                 #(default_code, color['text']),
                 #(cmp_2_cust_code['Elior'].get(
                 #    default_code), color['text']),  # Customer ELI code
                 #(cmp_2_cust_code['Dussmann'].get(
                 #    default_code), color['text']),  # Customer DUS code
                 #(name, color['text']),
-                #uom,
-                #(of_qty, black['number']),
-                #(net_qty, black['number']),
-                #(oc_qty, black['number']),
-                #(start_qty, black['number']),
+                uom,
+                (of_qty, black['number']),
+                (net_qty, black['number']),
+                (oc_qty, black['number']),
+                (start_qty, black['number']),
                 ], black['text'])
             # OF comment:
             if supplier_comment.get(default_code):
