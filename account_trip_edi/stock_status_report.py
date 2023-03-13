5723bd9993d594f70ac04f6068f86394b2ad9575
nicothebrush <nicola.riolini@gmail.com>
2023-03-13 11:10:27 +0100

    generic bugfix (fast message)

--- account_trip_edi/stock_status_report.py
+++ account_trip_edi/stock_status_report.py
@@ -731,9 +731,10 @@ class edi_company_report(orm.Model):
             else:
                 color = black
 
+            clean_code = clean_not_ascii_char(default_code)
             excel_pool.write_xls_line(ws_name, row, [
                 (u'Neg.' if has_negative else u'Pos.', color['text']),
-                (self.get_product_category(default_code), color['text']),
+                (self.get_product_category(clean_code), color['text']),
                 (default_code, color['text']),
                 (cmp_2_cust_code['Elior'].get(
                     default_code), color['text']),  # Customer ELI code
