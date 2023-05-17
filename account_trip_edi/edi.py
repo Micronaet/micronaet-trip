79eaefd99f46125bf9d071bf11c9b826bc6b39db
nicothebrush <nicola.riolini@gmail.com>
2023-05-17 12:16:22 +0200

    generic bugfix (fast message)

--- account_trip_edi/edi.py
+++ account_trip_edi/edi.py
@@ -132,8 +132,6 @@ class trip_import_edi_wizard(orm.Model):
             ('import', '=', True)], context=context)
         for company in edi_company_pool.browse(
                 cr, uid, edi_company_ids, context=context):
-            if company.name == 'SER':
-                pdb.set_trace()
             # Load object for use the same function name where needed:
             parametrized = self.pool.get(company.type_importation_id.object)
             _logger.warning('Reading %s...' % company.name)
